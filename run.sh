9044fc4f8f25ecc0a2734d063a69f2baf253bff5
Stanico Knowles <ashadknowles@gmail.com>
2023-07-16 16:03:20 -0400

    set condition so that command doesnt fail silently

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-set -a
-[ -f .env ] && source .env
-set +a
+# Check if .env file exists
+if [ -f .env ]; then
+  # Load environment variables from .env file
+  set -a
+  source .env
+  set +a
 
-# Run Go project
-go run server.go
+  # Run Go project
+  go run server.go
+else
+  echo "Error: .env file not found."
+fi
