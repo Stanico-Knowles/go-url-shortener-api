18f235f4f618027b7660d750430ef014ee0507bd
Stanico Knowles <ashadknowles@gmail.com>
2023-07-26 18:25:37 -0400

    add invalid request string to enums

--- src/url/enums/url_exceptions.go
+++ src/url/enums/url_exceptions.go
@@ -1,8 +1,9 @@
 package urlenums
 
 const (
-	URL_IS_REQUIRED string = "URL is required"
-	URL_NOT_FOUND   string = "URL not found"
-	URL_ALREADY_EXISTS string = "URL already exists"
+	INVALID_REQUEST      string = "Invalid request"
+	URL_IS_REQUIRED      string = "URL is required"
+	URL_NOT_FOUND        string = "URL not found"
+	URL_ALREADY_EXISTS   string = "URL already exists"
 	ALIAS_ALREADY_EXISTS string = "Alias already exists"
 )
